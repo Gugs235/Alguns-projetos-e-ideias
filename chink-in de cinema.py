0ec8a30be9f3799fcafecdc31fa276f76f863a45
Gugs235 <jhonathan.ssoares2008@gmail.com>
2024-10-16 17:40:50 -0300

    cinema

---
+++ chink-in de cinema.py
@@ -0,0 +1,8 @@
+# escolho o cinema
+
+print("cinema:")
+cinema = int(input("Escolha o cinema em que você deseja assitir: "))
+
+# escolho o filme
+# escolho o dia e horaio
+# escolho a forma de pagamento
