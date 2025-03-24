c143ca7360e279d4a97503d7d57af0860f1d6d88
Gugs235 <jhonathan.ssoares2008@gmail.com>
2025-03-24 15:16:05 -0400

    corrigindo a conexão com o banco

--- grok.ia/check in de cinema/frontend.py
+++ grok.ia/check in de cinema/frontend.py
@@ -7,7 +7,7 @@ import os
 from PySide6.QtMultimedia import QMediaPlayer
 from PySide6.QtMultimediaWidgets import QVideoWidget
 import yt_dlp
-from PyQt6.QtGui import QPixmap  # Adicionando QPixmap para trabalhar com imagens
+
 from PySide6.QtGui import QPixmap  # Importar QPixmap do PySide6
 import requests
 from PySide6.QtCore import QByteArray
